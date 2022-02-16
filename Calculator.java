b519fe0fd738d01691d71c920e6f74e35789cc83
NguyenThiThuTrang <99708445+tranggnguyen@users.noreply.github.com>
2022-02-16 07:09:14 +0700

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package moi;
 
-/**
- *
- * @author HoaiAnh
- */
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
